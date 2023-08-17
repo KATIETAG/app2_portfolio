c3febbcc04125c25c9b609509ba72eb5dd5111c8
KATIETAG <64670675+KATIETAG@users.noreply.github.com>
2023-08-17 15:12:53 -0700

    Define send email function

--- Pages/Contact_Us.py
+++ Pages/Contact_Us.py
@@ -1,13 +1,14 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key='email_forms'):
     user_email = st.text_input("Your Email address")
     message = st.text_area("Your Message")
+    message = message + "\n" + user_email
     button = st.form_submit_button("Submit")
 
     if button:
         # if button is presseed send an email
-        message = message + user_email
-        
+        send_email(message)
